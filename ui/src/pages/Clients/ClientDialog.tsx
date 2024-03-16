fa9b6cad9d463c2b14dde041c9dba0eddb6b0aca
XjnoBi <playmaker0720@gmail.com>
2024-03-17 00:22:03 +0800

    feat: add client form feat: integrate create endpoint

---
+++ ui/src/pages/Clients/ClientDialog.tsx
@@ -0,0 +1,143 @@
+import React from 'react';
+import {
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogTitle,
+	Grid,
+	Step,
+	StepLabel,
+	Stepper,
+	TextField,
+} from '@mui/material';
+import { Formik } from 'formik';
+import { ArrowBack } from '@mui/icons-material';
+import { createClient } from '../../services/api';
+
+type Props = {
+	client?: IClient;
+	open?: boolean;
+	onClose: (refetch?: boolean) => void;
+};
+
+const EMPTY_FORM: IClient = {
+	id: '',
+	email: '',
+	firstName: '',
+	lastName: '',
+	phoneNumber: '',
+};
+
+const STEPS = ['Personal details', 'Contact details'];
+
+const ClientDialog: React.FC<Props> = ({ client, open = false, onClose }) => {
+	const [activeStep, setActiveStep] = React.useState(0);
+	const onSubmit = async (values: IClient) => {
+		if (client?.id) {
+			//TODO: handle update procedure here...
+			return;
+		}
+
+		await createClient(values);
+		onClose(true);
+	};
+
+	return (
+		<Formik initialValues={client || EMPTY_FORM} onSubmit={onSubmit}>
+			{({ handleBlur, handleChange, handleSubmit, submitForm, values }) => {
+				const fieldProps = (name: keyof IClient) => ({
+					fullWidth: true,
+					onBlur: handleBlur,
+					onChange: handleChange,
+					name,
+					value: values[name],
+				});
+				const getFormFields = () => {
+					switch (activeStep) {
+						case 0:
+							return (
+								<React.Fragment>
+									<Grid item>
+										<TextField {...fieldProps('firstName')} label='First name' />
+									</Grid>
+									<Grid item>
+										<TextField {...fieldProps('lastName')} label='Last name' />
+									</Grid>
+								</React.Fragment>
+							);
+						case 1:
+							return (
+								<React.Fragment>
+									<Grid item>
+										<TextField {...fieldProps('email')} label='Email' />
+									</Grid>
+									<Grid item>
+										<TextField {...fieldProps('phoneNumber')} label='Phone number' />
+									</Grid>
+								</React.Fragment>
+							);
+					}
+				};
+				const getActions = () => {
+					switch (activeStep) {
+						case 0:
+							return (
+								<Button variant='contained' onClick={() => setActiveStep(1)}>
+									Continue
+								</Button>
+							);
+						case 1:
+							return (
+								<Grid container justifyContent='space-between'>
+									<Grid item>
+										<Button
+											variant='text'
+											onClick={() => setActiveStep(0)}
+											startIcon={<ArrowBack />}
+										>
+											Back
+										</Button>
+									</Grid>
+									<Grid item>
+										<Button variant='contained' onClick={() => submitForm()}>
+											Create client
+										</Button>
+									</Grid>
+								</Grid>
+							);
+					}
+				};
+
+				return (
+					<Dialog open={open} onClose={() => onClose()} fullWidth={true} maxWidth='sm'>
+						<form onSubmit={handleSubmit}>
+							<DialogTitle>{client ? 'Update client' : 'Create new client'}</DialogTitle>
+							<DialogContent>
+								<Grid container direction='column' spacing={4}>
+									<Grid item>
+										<Stepper activeStep={activeStep}>
+											{STEPS.map((label, index) => (
+												<Step key={index}>
+													<StepLabel>{label}</StepLabel>
+												</Step>
+											))}
+										</Stepper>
+									</Grid>
+									<Grid item>
+										<Grid container alignItems='stretch' direction='column' spacing={2}>
+											{getFormFields()}
+										</Grid>
+									</Grid>
+								</Grid>
+							</DialogContent>
+							<DialogActions>{getActions()}</DialogActions>
+						</form>
+					</Dialog>
+				);
+			}}
+		</Formik>
+	);
+};
+
+export default ClientDialog;
--- ui/src/pages/Clients/index.tsx
+++ ui/src/pages/Clients/index.tsx
@@ -1,30 +1,51 @@
-import { memo, useContext, useEffect } from "react";
-import { Paper, Typography } from "@mui/material";
-import { StateContext } from "../../store/DataProvider";
-import Page from "../../components/Page";
-import ClientTable from "./ClientTable";
-import { getClients } from "../../services/api";
+import { memo, useContext, useEffect, useState } from 'react';
+import { Button, Grid, Paper, TextField, Typography } from '@mui/material';
+import { StateContext } from '../../store/DataProvider';
+import Page from '../../components/Page';
+import ClientTable from './ClientTable';
+import { getClients } from '../../services/api';
+import ClientDialog from './ClientDialog';
 
 function Clients() {
-  const { state, dispatch } = useContext(StateContext);
-  const { clients } = state;
+	const { state, dispatch } = useContext(StateContext);
+	const { clients } = state;
 
-  useEffect(() => {
-    getClients().then((clients) =>
-      dispatch({ type: "FETCH_ALL_CLIENTS", data: clients })
-    );
-  }, [dispatch]);
+	const [showDialog, setShowDialog] = useState(false);
+	const [refetchCounter, setRefetchCounter] = useState(0);
 
-  return (
-    <Page>
-      <Typography variant="h4" sx={{ textAlign: "start" }}>
-        Clients
-      </Typography>
-      <Paper sx={{ margin: "auto", marginTop: 3 }}>
-        <ClientTable clients={clients} />
-      </Paper>
-    </Page>
-  );
+	const handleCloseDialog = (refetch?: boolean) => {
+		if (refetch) {
+			setRefetchCounter(refetchCounter + 1);
+		}
+
+		setShowDialog(false);
+	};
+
+	useEffect(() => {
+		getClients().then((clients) => dispatch({ type: 'FETCH_ALL_CLIENTS', data: clients }));
+	}, [dispatch, refetchCounter]);
+
+	return (
+		<Page>
+			<Typography variant='h4' sx={{ textAlign: 'start' }}>
+				Clients
+			</Typography>
+			<Grid container alignItems='center' justifyContent='space-between'>
+				<Grid item>
+					<TextField placeholder='Search client...' />
+				</Grid>
+				<Grid item>
+					<Button variant='contained' onClick={() => setShowDialog(true)}>
+						Create new client
+					</Button>
+				</Grid>
+			</Grid>
+			<Paper sx={{ margin: 'auto', marginTop: 3 }}>
+				<ClientTable clients={clients} />
+			</Paper>
+			{showDialog && <ClientDialog open onClose={handleCloseDialog} />}
+		</Page>
+	);
 }
 
 export default memo(Clients);
